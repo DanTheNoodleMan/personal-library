958aacbc6655fb9a5cb142ecdfffa6badf6dead7
Daniel Robertson <31508473+DanTheNoodleMan@users.noreply.github.com>
2023-07-24 13:46:55 +0200

    Made the constructor, created some sample book objects and made render function to display the books in the array

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-let myLibrary = [];
-
-function Book() {
-  // the constructor...
-  
+function Book(name, author, pages, read) {
+    // the constructor...
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
+
 }
+
+const memoirsGeisha = new Book('Memoirs of a Geisha', 'Arthur Golden', 448, true);
+const harryPotter = new Book('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 223, true);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, true);
+
+let myLibrary = [memoirsGeisha, harryPotter, theHobbit];
+
+let bookContainer = document.querySelector('.book-container');
+
+function render() {
+    // do stuff here
+    myLibrary.forEach((book) => {
+        // create a card for each book
+        bookCard = document.createElement('div');
+        bookCard.textContent = book.name;
+        bookCard.classList.add('book-card');
+        bookContainer.appendChild(bookCard);
+        console.log(book);
+    });
+}
+
+render();
